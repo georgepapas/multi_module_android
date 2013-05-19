6aecf34f9501e0738091867c87c99406d0929b50
George Papas <george.papas@gmail.com>
2013-05-19 19:51:34 +1000

    initial commit

---
+++ drdroid-it/src/main/java/com/hsp/drdroid/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.hsp.drdroid.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.hsp.drdroid.HelloAndroidActivity;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
