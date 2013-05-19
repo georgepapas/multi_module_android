c0d9e1d5ffcc233b02161b89207746ccd7f65cc0
George Papas <george.papas@gmail.com>
2013-05-19 20:25:41 +1000

    enabled checkstyle

--- drdroid-it/src/main/java/com/hsp/drdroid/test/HelloAndroidActivityTest.java
+++ drdroid-it/src/main/java/com/hsp/drdroid/test/HelloAndroidActivityTest.java
@@ -5,13 +5,12 @@ import com.hsp.drdroid.HelloAndroidActivity;
 
 public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
 
-    public HelloAndroidActivityTest() {
-        super(HelloAndroidActivity.class); 
-    }
+  public HelloAndroidActivityTest() {
+    super(HelloAndroidActivity.class);
+  }
 
     public void testActivity() {
         HelloAndroidActivity activity = getActivity();
         assertNotNull(activity);
     }
 }
-
