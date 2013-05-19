cad71fb9b6744278aec7a422c996054f611b0f74
George Papas <george.papas@gmail.com>
2013-05-19 21:04:30 +1000

    Added spoon integration

--- drdroid-it/src/main/java/com/hsp/drdroid/test/HelloAndroidActivityTest.java
+++ drdroid-it/src/main/java/com/hsp/drdroid/test/HelloAndroidActivityTest.java
@@ -2,6 +2,7 @@ package com.hsp.drdroid.test;
 
 import android.test.ActivityInstrumentationTestCase2;
 import com.hsp.drdroid.HelloAndroidActivity;
+import com.squareup.spoon.Spoon;
 
 public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
 
@@ -9,8 +10,10 @@ public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<H
     super(HelloAndroidActivity.class);
   }
 
-    public void testActivity() {
-        HelloAndroidActivity activity = getActivity();
-        assertNotNull(activity);
-    }
+  public void testActivity() {
+    Spoon.screenshot(getActivity(), "Hello_Android_activity__initial_state");
+
+    HelloAndroidActivity activity = getActivity();
+    assertNotNull(activity);
+  }
 }
